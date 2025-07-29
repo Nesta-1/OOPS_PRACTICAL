8eace70240a001eeb513421f8fabba4224d14b9d
Nesta-1 <2420080070@klh.edu.in>
2025-07-29 11:39:21 +0530

    Added file SubstringExample2.java

---
+++ SubstringExample2.java
@@ -0,0 +1,12 @@
+package OOPS_PRACTICAL;
+
+public class SubstringExample2 {
+
+	public static void main(String[] args) {
+		String s1="JavaProgramming";
+		String sub=s1.substring(4,11);
+		System.out.println("Substring: "+sub);
+
+	}
+
+}
