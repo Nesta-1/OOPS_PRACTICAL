089ab7c80dc54576769425114abdd8a8fbc2ab61
Nesta-1 <2420080070@klh.edu.in>
2025-07-29 11:30:45 +0530

    Added Charlength.java file

---
+++ Charlength.java
@@ -0,0 +1,12 @@
+package OOPS_PRACTICAL;
+
+public class Charlength {
+
+	public static void main(String[] args) {
+		String s="java programs";
+		System.out.println("charAt(2): "+s.charAt(2));
+		System.out.println("length(): "+s.length());
+
+	}
+
+}
