fdc74b7a6ca757579f77816fdcc4c190c3246197
Nesta-1 <2420080070@klh.edu.in>
2025-07-29 11:31:31 +0530

    Add files via upload
    Added substringExample.java

---
+++ substringExample.java
@@ -0,0 +1,12 @@
+package OOPS_PRACTICAL;
+
+public class substringExample {
+
+	public static void main(String[] args) {
+		String s="Hello World";
+		System.out.println(s.substring(5));
+		System.out.println(s.substring(2,7));
+
+	}
+
+}
