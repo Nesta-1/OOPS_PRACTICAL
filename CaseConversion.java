d4c5cf8c3c13730ed4c5caa7e1ad3f7f4d2ec31c
Nesta-1 <2420080070@klh.edu.in>
2025-07-29 11:34:13 +0530

    Added CaseConversion.java

---
+++ CaseConversion.java
@@ -0,0 +1,12 @@
+package OOPS_PRACTICAL;
+
+public class CaseConversion {
+
+	public static void main(String[] args) {
+		String s="hello world";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+
+	}
+
+}
