535cbc7973bea436a16d111002e4db69a70c2f4c
Nesta-1 <2420080070@klh.edu.in>
2025-08-05 10:57:45 +0530

    Practiced a basic constructor example

---
+++ ConstructorExample.java
@@ -0,0 +1,23 @@
+package OOPS_PRACTICAL;
+
+class Student {
+    String name;
+    int age;
+
+    // Constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+public class ConstructorExample {
+    public static void main(String[] args) {
+        Student s1 = new Student("Rose", 67);
+        s1.display();
+    }
+}
